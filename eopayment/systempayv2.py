18ce89571b1344a3c092808fd24d0ed203900478
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2016-02-16 18:26:20 +0100

    systempayv2: extract tests from module

--- eopayment/systempayv2.py
+++ eopayment/systempayv2.py
@@ -4,7 +4,6 @@ import datetime as dt
 import hashlib
 import logging
 import string
-import urlparse
 import warnings
 from gettext import gettext as _
 
@@ -411,15 +410,3 @@ class Payment(PaymentCommon):
         sign = hashlib.sha1(signed_data).hexdigest()
         self.logger.debug('signature «%s»' % sign)
         return sign
-
-if __name__ == '__main__':
-    # Test vector from Systempayv2 documentation
-    p = Payment(dict(secret_test='1122334455667788'))
-    qs = 'vads_version=V2&vads_page_action=PAYMENT&vads_action_mode=INTERACTIV' \
-         'E&vads_payment_config=SINGLE&vads_site_id=12345678&vads_ctx_mode=TES' \
-         'T&vads_trans_id=654321&vads_trans_date=20090501193530&vads_amount=15' \
-         '24&vads_currency=978'
-    qs = urlparse.parse_qs(qs)
-    for key in qs.keys():
-        qs[key] = qs[key][0]
-    assert p.signature(qs) == '606b369759fac4f0864144c803c73676cbe470ff'
