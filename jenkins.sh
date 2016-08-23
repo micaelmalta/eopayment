5362339bbb960be48bc3dfc7139fead547646cba
Frédéric Péters <fpeters@entrouvert.com>
2016-08-23 08:00:52 +0200

    jenkins: install latest pylint

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@
 set -e
 
 pip install --upgrade pip
-pip install --upgrade pylint==1.4.0 astroid==1.3.2
+pip install --upgrade pylint pylint-django
 pip install --upgrade tox
 (pylint -f parseable --rcfile /var/lib/jenkins/pylint.django.rc eopayment | tee pylint.out) || /bin/true
 tox -r
